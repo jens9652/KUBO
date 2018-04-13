fa40f5e0d4c9cf395bb78a2eb2e43f8ac33796e4
Mathias Kastrup <mathias@noeka.dk>
2018-04-13 04:16:43 +0200

    Accordion does not need to be an object.

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,7 +1,4 @@
-function Accordion (accordion) {
-}
-
-Accordion.prototype.toggleItem = function(element) {
+toggleAccordionItem = function(element) {
   var itemClass = element.parentNode.className;
   var accordion = document.getElementById('accordion');
   var accordionItems = accordion.getElementsByClassName('accordion-item');
@@ -12,10 +9,4 @@ Accordion.prototype.toggleItem = function(element) {
   if (itemClass == 'accordion-item close') {
     element.parentNode.className = 'accordion-item open';
   }
-}
-
-var item = document.getElementById('accordion');
-
-if (item) {
-  var accordion = new Accordion(item);
 }
