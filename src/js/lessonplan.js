fa5593774284e22a2f4f6f5396456955f5a47021
Mathias Kastrup <mathias@noeka.dk>
2018-04-13 07:18:18 +0200

    Removed console error if no lessonplans were present.

--- src/js/lessonplan.js
+++ src/js/lessonplan.js
@@ -85,29 +85,30 @@ Lessonplan.prototype.publish = function() {
 Lessonplan.prototype.listLessons = function(element) {
   var lessonPlans = JSON.parse(localStorage.getItem('lessonPlans'));
 
-  for (var i = 0; i < lessonPlans.length; i++) {
-    var div = document.createElement('div');
-    div.className = 'lesson';
+  if (lessonPlans) {
+    for (var i = 0; i < lessonPlans.length; i++) {
+      var div = document.createElement('div');
+      div.className = 'lesson';
 
-    var image = lessonPlans[i].image;
+      var image = lessonPlans[i].image;
 
-    if (!image) {
-      image = 'public/images/Coding-box-open-2.jpg';
+      if (!image) {
+        image = 'public/images/Coding-box-open-2.jpg';
+      }
+
+      div.innerHTML = 
+        '<div class="inner" style="background-image: url(&quot;' + image + '&quot;)">\
+          <div class="overlay"></div>\
+          <div class="text">\
+            <h3>' + lessonPlans[i].title + '</h3>\
+            <p>' + lessonPlans[i].description + '</p>\
+            <a href="view-lessonplan.html" onclick="controller.setLessonplan(' + lessonPlans[i].id + ')" class="btn btn-primary">View Lesson Plan</a>\
+          </div>\
+        </div>'
+
+      element.appendChild(div);
     }
-
-    div.innerHTML = 
-      '<div class="inner" style="background-image: url(&quot;' + image + '&quot;)">\
-        <div class="overlay"></div>\
-        <div class="text">\
-          <h3>' + lessonPlans[i].title + '</h3>\
-          <p>' + lessonPlans[i].description + '</p>\
-          <a href="view-lessonplan.html" onclick="controller.setLessonplan(' + lessonPlans[i].id + ')" class="btn btn-primary">View Lesson Plan</a>\
-        </div>\
-      </div>'
-
-    element.appendChild(div);
   }
-
 }
 
 Lessonplan.prototype.setLessonplan = function(id) {
