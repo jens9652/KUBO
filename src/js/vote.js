33107b703029286186f0b245ee73358245a8d5a5
jens9652 <32540877+jens9652@users.noreply.github.com>
2018-04-16 12:50:34 +0200

    Added JavaScript for voting and more
    Added js to upvote and downvote - implemented in the HTML on communty post and training

---
+++ src/js/vote.js
@@ -0,0 +1,7 @@
+function upVote(event) {
+  event.target.parentElement.classList.add('btn-clicked-upvote');
+}
+
+function downVote(event) {
+  event.target.parentElement.classList.add('btn-clicked-downvote');
+}
