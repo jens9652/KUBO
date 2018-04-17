1a40773f2bfbfbbca2e3b845647d24f85c0356a0
Mathias Kastrup <mathias@noeka.dk>
2018-04-17 12:32:08 +0200

    Final commit.

--- src/js/profile.js
+++ src/js/profile.js
@@ -53,7 +53,7 @@ ProfileController.prototype.showLessonPlans = function(id) {
 		        <div class="overlay"></div>\
 		        <div class="text">\
 		          <h3>' + lessonPlans[i].title + '</h3>\
-		          <p>' + lessonPlans[i].description + '</p>\
+		          <p>' + lessonPlans[i].description.substring(0, 110) + '...</p>\
 		          <a href="view-lessonplan.html" onclick="controller.setLessonplan(' + lessonPlans[i].id + ')" class="btn btn-primary">View Lesson Plan</a>\
 		        </div>\
 		      </div>'
